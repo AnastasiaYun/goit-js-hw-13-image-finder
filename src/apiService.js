700cc796f2dc9ce2699f5c7a532dab39e8ef9624
AnastasiaYun <tigrawom@gmail.com>
2021-05-31 23:11:14 +0300

    1

---
+++ src/apiService.js
@@ -0,0 +1,32 @@
+const BASE_URL = 'https://pixabay.com/api';
+const API_KEY = '21756599-0afd71203aca16b66ad6b1f5f';
+
+export default class NewsApiService {
+    constructor() {
+        this.inputValue = '';
+        this.pageNumber = 1;
+        
+    };
+
+    fetchImages() {
+        const url = `${BASE_URL}/?image_type=photo&orientation=horizontal&q=${this.inputValue}&page=${this.pageNumber}&per_page=12&key=${API_KEY}`;
+
+        return fetch(url).then(response => response.json()).then(this.incrementPage());
+    };
+
+    incrementPage() {
+        this.pageNumber += 1;
+    };
+
+    resetPage() {
+    this.pageNumber = 1;
+  }
+
+    get query() {
+        return this.inputValue;
+    };
+
+    set query(newValue) {
+        this.inputValue = newValue;
+    };
+ };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,83 @@
-import './sass/main.scss';
+
+import * as basicLightbox from 'basiclightbox';
+import debounce from 'lodash.debounce';
+import { alert, notice, info, success, error } from '@pnotify/core';
+import { defaults } from '@pnotify/core';
+
+
+import NewsApiService from './apiService.js';
+
+import imagesCardTpl from './templates/image-card.hbs';
+import modalImageTpl from './templates/modal-image.hbs';
+
+
+
+
+const refs = {
+    inputSearch: document.querySelector("#search-form input"),
+    galleryLists: document.querySelector('.gallery'),
+    loadMoreBtn: document.querySelector('[data-action="load-more"]'),
+
+
+};
+
+const newsApiService = new NewsApiService();
+
+
+refs.inputSearch.addEventListener('input', debounce(onSearch, 500));
+refs.loadMoreBtn.addEventListener('click', loadMoreArticles);
+
+
+function onSearch(e) {
+     e.preventDefault();
+
+    newsApiService.query = refs.inputSearch.value;
+
+    if (newsApiService.query.trim() === '') {
+        error();
+        return;
+        
+    };
+    newsApiService.resetPage();
+    clearArticlesContainer();
+    fetchArticles();
+    refs.loadMoreBtn.classList.remove('is-hidden')
+    
+  
+};
+
+function fetchArticles() {
+    info()
+     newsApiService.fetchImages().then(articles => { renderImagesCard(articles)});
+    
+};
+
+function loadMoreArticles() {
+   
+    fetchArticles();
+    scrollImages();
+        
+};
+
+function renderImagesCard(articles) {
+    const markup = imagesCardTpl(articles.hits);
+
+    refs.galleryLists.insertAdjacentHTML('beforeend', markup);
+};
+
+function scrollImages() {
+    
+
+
+ const element = document.querySelector('.gallery');
+    setTimeout(() => {
+        window.scrollTo({
+         top: element.clientHeight,
+         behavior: 'smooth',
+       });
+    }, 500);
+};
+function clearArticlesContainer() {
+  refs.galleryLists.innerHTML = '';
+}
+
